8bbf40675fe7d614c607a41c41b26ac1e905aa5f
dangshen <mum8u6@gmail.com>
2020-01-11 06:35:58 -0500

    新增图片上传功能

--- miniprogram/pages/timeTable/timeTable.js
+++ miniprogram/pages/timeTable/timeTable.js
@@ -12,22 +12,23 @@ Page({
     imagePath_edit: "/images/edit.png",
     imagePath_menu: "/images/menu.png",
     time: ["8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00",
-           "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", 
-           "22:00"],
+      "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00",
+      "22:00"
+    ],
     weekdays: ["Mon", "Tue", "Wed", "Thu", "Fri"],
     courseList: [],
-    showMenu : false
+    showMenu: false
   },
 
   //展示侧边菜单栏
-  showLeftMenu: function(){
+  showLeftMenu: function () {
     this.setData({
-      showMenu:true
+      showMenu: true
     })
   },
 
   //隐藏侧边菜单栏
-  hideLeftMenu: function(){
+  hideLeftMenu: function () {
     this.setData({
       showMenu: false
     })
@@ -44,7 +45,6 @@ Page({
         console.log('[云函数] [login] user openid: ', res.result.openid);
         app.globalData.openid = res.result.openid;
         this.getUserCourseInfo();
-
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
@@ -52,50 +52,60 @@ Page({
     })
   },
   // 获取当前用户的课程信息
-  getUserCourseInfo: function(){
-    wx.showLoading({
-    });
+  getUserCourseInfo: function () {
+    wx.showLoading({});
     db.collection('course').where({
       _openid: app.globalData.openid
-    }).get().then(res=>{
+    }).get().then(res => {
       if (res.data.length === 0) {
-        Notify({ type: 'primary', message: 'No Course，Please Add' });
+        Notify({
+          type: 'primary',
+          message: 'No Course，Please Add'
+        });
       }
       this.setData({
         courseList: this.data.courseList.concat(res.data)
       });
       wx.hideLoading();
-    }).catch(err=>{
-      Notify({ type: 'danger', message: 'Sever Error, Please Contact WeChat mum8u6' });
+    }).catch(err => {
+      Notify({
+        type: 'danger',
+        message: 'Sever Error, Please Contact WeChat mum8u6'
+      });
       wx.hideLoading();
     })
   },
-  
+
   //插入课程信息
-  insertCourse: function(){
+  insertCourse: function () {
     db.collection('course').add({
-      data: {
-      }
-    }).then(res=>{
+      data: {}
+    }).then(res => {
       console.log(res)
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err)
     })
   },
 
-  editCourse: function(){
+  editCourse: function () {
     wx.navigateTo({
       url: '../course/courseList',
     })
   },
 
+  toUpload:function(){
+    wx.navigateTo({
+      url: '../upload/upload',
+    })
+  },
+ 
   shouwMap: function () {
     wx.navigateTo({
       url: '../map/map',
     })
   },
 
-  toFollowList:function(){
+  toFollowList: function () {
     wx.navigateTo({
       url: '../follow/followList',
     })
@@ -103,68 +113,67 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
-    
+  onLoad: function (options) {
+    this.getOpenid();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
+    console.log('onShow')
     this.setData({
       courseList: []
     });
     this.getOpenid();
-  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function(res) {
+  onShareAppMessage: function (res) {
     if (res.from === 'menu') {
       return {
         title: 'Ac TimeTable',
         path: `/pages/timeTable/timeTable`
       }
-    }else{
-      
+    } else {
+
       this.setData({
-        showMenu:false
+        showMenu: false
       });
       return {
         title: 'Share My TimeTable',
---
+++ miniprogram/pages/upload/upload.js
@@ -0,0 +1,161 @@
+import Notify from '@vant/weapp/notify/notify';
+
+const db = wx.cloud.database();
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    showLoading: false
+  },
+
+   //上传图片至云存储
+   uploadImg: function () {
+    const openid = app.globalData.openid;
+    let that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths;
+
+        that.setData({
+          showLoading: true
+        })
+        // 将图片上传至云存储空间
+        wx.cloud.uploadFile({
+          // 指定上传到的云路径
+          cloudPath: `${openid}.png`,
+          // 指定要上传的文件的小程序临时文件路径
+          filePath: tempFilePaths[0],
+          // 成功回调
+          success: res => {
+            //返回文件ID
+            const fileID = res.fileID;
+            //原计划先查询数据库中是否存在当前用户的图片信息，如果存在需要删除后再保存，确保一个用户只有一个图片信息
+            //但是经测试发现，同一用户，上传名称相同的图片后，图片内容会覆盖，但是fileID不变，因此不需要删除原来的记录，只需要在无记录的情况下添加一条记录即可
+            //仍旧保持数据库中针对同一用户只有一条图片记录
+            db.collection('image').where({
+              _openid: openid
+            }).get().then(res => {
+              console.log(res);
+              if (res.data.length === 0) {
+                  //保存图片信息到数据库
+                  db.collection('image').add({
+                    data: {
+                      fileId: fileID
+                    }
+                  }).then(res => {
+                    Notify({
+                      type: 'success',
+                      message: 'Upload Successfully!'
+                    });
+                    that.setData({
+                      showLoading: false
+                    })
+                  }).catch(err => {
+                    Notify({
+                      type: 'danger',
+                      message: 'Sever Error, Please Contact WeChat mum8u6'
+                    });
+                    console.log(err);
+                    that.setData({
+                      showLoading: false
+                    })
+                  })
+              }else{
+                Notify({
+                  type: 'success',
+                  message: 'Upload Successfully!'
+                });
+                that.setData({
+                  showLoading: false
+                })
+              }
+              
+            }).catch(err => {
+              console.log(err);
+              Notify({
+                type: 'danger',
+                message: 'Sever Error, Please Contact WeChat mum8u6'
+              });
+              that.setData({
+                showLoading: false
+              })
+            });
+          },
+          fail: err => {
+            console.log(err);
+            Notify({
+              type: 'danger',
+              message: 'Sever Error, Please Contact WeChat mum8u6'
+            });
+            that.setData({
+              showLoading: false
+            })
+          }
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
